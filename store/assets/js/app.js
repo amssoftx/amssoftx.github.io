34404debc43efb1826c48f0eb25f33efa9b312e2
AMSSoftX <amssoftx@gmail.com>
2026-07-20 21:51:22 +0530

    Update app.js

--- store/assets/js/app.js
+++ store/assets/js/app.js
@@ -1,1 +1,31 @@
-console.log("AMS Store Loaded");
+// ================================
+// AMSSoftX Store Engine v1
+// ================================
+
+const STORE_NAME = "AMSSoftX Store";
+
+console.log(STORE_NAME + " Loaded");
+
+async function loadProducts() {
+
+    try {
+
+        const response = await fetch("data/products.json");
+
+        const products = await response.json();
+
+        console.log(products);
+
+    }
+
+    catch(error){
+
+        console.error("Products Loading Error");
+
+        console.error(error);
+
+    }
+
+}
+
+loadProducts();
